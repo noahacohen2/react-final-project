988f6dd127385266c003e5fd441fdb833aa5843a
noaHacohen <noanoa112@gmail.com>
2023-01-24 13:34:43 +0200

    first initialize

---
+++ server/inedx.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+const app = express();
+const cors = require("cors");
+
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/assignment', { useNewUrlParser: true })
+    .then(() => {
+        console.log("mongo connection open")
+    }
+    ).catch(err => {
+        console.log("mongo connection error", err)
+    })
+
+
+
+app.listen(3000, () => {
+    console.log("listening in port 3000")
+})
