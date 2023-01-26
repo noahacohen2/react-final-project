d7272b5d3371f3df05218fb1330e70e1ea531c11
Daniel Sabag <daniel108s17b@gmail.com>
2023-01-26 23:30:03 +0200

    add search and filter UI

--- client/src/Pages/MusicalsPage/MusicalsPage.js
+++ client/src/Pages/MusicalsPage/MusicalsPage.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import MusicalCard from "../../Components/MusicalCard/MusicalCard";
+import FilterAltOutlinedIcon from "@mui/icons-material/FilterAltOutlined";
 import AppContext from "../../Context/Context.js";
 import musicalsService from "../../services/musicals.js";
 import { useContext, useEffect } from "react";
@@ -34,6 +35,25 @@ const MusicalsPage = () => {
         alignItems="flex-start"
         id="musical-page-title"
       >
+        <div id="actions-area">
+          <Grid
+            container
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+          >
+            <div id="search">
+              <TextField
+                fullWidth
+                size="small"
+                color="primary"
+                variant="outlined"
+                label="Search"
+              />
+            </div>
+            <FilterAltOutlinedIcon id="filter-icon" fontSize="large" />
+          </Grid>
+        </div>
         <h1>Musicals in London and New York You Can't Miss</h1>
         <div>Find the best musical for you</div>
       </Grid>
